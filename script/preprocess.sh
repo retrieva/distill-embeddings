6718221c0804190afd7f744136a8e36457945c55
yano0 <yanonay3@gmail.com>
2025-08-06 10:13:09 +0900

    データの構築

--- script/preprocess.sh
+++ script/preprocess.sh
@@ -3,8 +3,9 @@
 uv run python src/preprocess/data_encode.py \
     --data_name "hotchpotch/fineweb-2-edu-japanese" \
     --subset_name "sample_10BT" \
-    --sample_size 50 \
+    --sample_size 100000 \
     --output_dir "data" \
     --teacher_model "Qwen/Qwen3-Embedding-4B" \
-    --batch_size 8 \
-    --max_length 8192
+    --long_batch_size 1 \
+    --short_batch_size 16 \
+    --max_length 4096 \
--- src/preprocess/data_encode.py
+++ src/preprocess/data_encode.py
@@ -6,6 +6,13 @@ from pathlib import Path
 import torch
 import numpy as np
 import json
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s: %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 def split_sentences(sentences, max_length):
     result = []
@@ -72,31 +79,60 @@ def main(args):
     )
     if args.sample_size:
         dataset = dataset.take(args.sample_size)
-    print(f"Loaded dataset: {args.data_name} ({args.subset_name}) with {args.sample_size} samples")
+    logger.info(f"Loaded dataset: {args.data_name} ({args.subset_name}) with {args.sample_size} samples")
     texts = list(dataset['text'])
     if args.short_text_ratio > 0:
         texts = split_to_short_texts(texts, args.short_text_ratio)
-    print(f"Number of texts after splitting: {len(texts)}")
-    texts = sequential_sort(texts)
+    # 後ろの方にめっちゃ短いの集まってそうなので、削っとく
+    drop_num = int(len(texts) * 0.01)
+    texts = sequential_sort(texts)[:-drop_num]
+    logger.info(f"Number of texts after splitting: {len(texts)}")
     # Print statistics about the distribution of text lengths
     stats = stat_text_lengths(texts)
-    print("Text length statistics:", stats)
-    
+    logger.info(f"Text length statistics: {stats}")
+    long_texts = [text for text in texts if len(text) > args.threshold]
+    short_texts = [text for text in texts if len(text) <= args.threshold]
     teacher_model = SentenceTransformer(args.teacher_model).bfloat16()
-    print(f"Loaded teacher model: {args.teacher_model}")
+    logger.info(f"Loaded teacher model: {args.teacher_model}")
+
     with torch.no_grad():
-        # Iterate through the dataset and print each sample
-        teacher_features = teacher_model.encode(
-            texts, show_progress_bar=True, 
-            batch_size=args.batch_size,
+        logger.info("-- Check OOM --")
+        teacher_model.encode(
+            long_texts[:2],
+            show_progress_bar=True, 
+            batch_size=args.long_batch_size,
             max_length=args.max_length,
-            convert_to_tensor=True,
-            normalize_embeddings=True)
+            normalize_embeddings=True,
+        )
+        teacher_model.encode(
+            short_texts[:32],
+            show_progress_bar=True, 
+            batch_size=args.short_batch_size,
+            max_length=args.max_length,
+            normalize_embeddings=True,
+        )
+        logger.info("-- Long Encode --")
+        # Iterate through the dataset and print each sample
+        long_teacher_features = teacher_model.encode(
+            long_texts,
+            show_progress_bar=True, 
+            batch_size=args.long_batch_size,
+            max_length=args.max_length,
+            normalize_embeddings=True,
+        )
+        logger.info("-- Short Encode --")
+        short_teacher_features = teacher_model.encode(
+            short_texts,
+            show_progress_bar=True, 
+            batch_size=args.short_batch_size,
+            max_length=args.max_length,
+            normalize_embeddings=True,
+            )
     output_dataset = Dataset.from_dict({
-        'text': texts,
-        'teacher_features': teacher_features.tolist()
+        'text': long_texts + short_texts,
+        'teacher_features': np.concatenate([long_teacher_features, short_teacher_features]),
     })
-    output_path = Path(args.output_dir) / f"{args.teacher_model.replace('/', '_')}_encoded"
+    output_path = Path(args.output_dir) / f"{args.teacher_model.replace('/', '_')}_encoded" / f"{args.subset_name}_{args.sample_size if args.sample_size else 'full'}"
     output_path.mkdir(parents=True, exist_ok=True)
     output_dataset.save_to_disk(output_path)
     json.dump(stats, open(output_path / "stats.json", "w"), indent=4)
@@ -108,9 +144,10 @@ if __name__ == "__main__":
     parser.add_argument("--output_dir", type=str, required=True, help="Path to save the output directory")
     parser.add_argument("--teacher_model", type=str, required=True, help="Path to the teacher model")
     parser.add_argument("--sample_size", type=int, default=None, help="Number of samples to load")
-    parser.add_argument("--batch_size", type=int, default=32, help="Batch size for processing")
+    parser.add_argument("--long_batch_size", type=int, default=1, help="Batch size for processing long texts")
+    parser.add_argument("--short_batch_size", type=int, default=32, help="Batch size for processing short texts")
     parser.add_argument("--max_length", type=int, default=None, help="Maximum length for tokenization")
-
+    parser.add_argument("--threshold", type=int, default=4096, help="Threshold for distinguishing long and short texts 文字数なのに注意")
     parser.add_argument("--short_text_ratio", type=float, default=0.3, help="Ratio of short text samples")
     args = parser.parse_args()
     main(args)
