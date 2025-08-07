af137b3a447cc861dc08ae2a71117d1fa9492a0a
yano0 <yanonay3@gmail.com>
2025-08-07 18:25:45 +0900

    評価

--- script/eval.sh
+++ script/eval.sh
@@ -1,6 +1,8 @@
 
-models=("/home/chihiro_yano/work/distill-embeddings/output/cl-nagoya_ruri-v3-pt-30m/Qwen_Qwen3-Embedding-4B/sample_10BT_100/e1_bs2_ckd/distillation/myfkz6mp/checkpoints/last.ckpt")
-# models=("cl-nagoya/ruri-v3-pt-30m" "cl-nagoya/ruri-v3-pt-70m" "cl-nagoya/ruri-v3-pt-130m" "cl-nagoya/ruri-v3-pt-310m")
+# models=("/home/chihiro_yano/work/distill-embeddings/output/cl-nagoya_ruri-v3-pt-30m/Qwen_Qwen3-Embedding-4B/sample_10BT_1000/e3_bs16_lr5e-05_taid-ckd/distillation/2zqc9nu4/checkpoints/last.ckpt")
+# models=("/home/chihiro_yano/work/distill-embeddings/output/cl-nagoya_ruri-v3-pt-30m/Qwen_Qwen3-Embedding-4B/sample_10BT_1000/e3_bs16_lr5e-05_taid-kld/distillation/dwp09prg/checkpoints/last.ckpt")
+# models=("/home/chihiro_yano/work/distill-embeddings/output/cl-nagoya_ruri-v3-pt-30m/Qwen_Qwen3-Embedding-4B/sample_10BT_1000/e3_bs16_lr5e-05_taid-mse/distillation/rygs174q/checkpoints/last.ckpt")
+models=("cl-nagoya/ruri-v3-pt-30m" "Qwen/Qwen3-Embedding-4B")
 # models=("cl-nagoya/ruri-v3-30m" "cl-nagoya/ruri-v3-70m" "cl-nagoya/ruri-v3-130m" "cl-nagoya/ruri-v3-310m")
 # models=("Qwen/Qwen3-Embedding-0.6B" "Qwen/Qwen3-Embedding-4B" "Qwen/Qwen3-Embedding-8B")
 
