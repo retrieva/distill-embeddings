48e9ce6185d4c7344eae2beec85d941a8daa462c
yano0 <yanonay3@gmail.com>
2025-08-07 18:25:35 +0900

    色々評価

--- eval.py
+++ eval.py
@@ -16,23 +16,36 @@ def main(args):
     else:
         # 2. SentenceTransformerモデルを直接ロード
         model = SentenceTransformer(model_name)
-        output_folder = "output" + model_name.replace("/", "_")
+        output_folder = Path("output") / model_name.replace("/", "_")
     evaluation = mteb.MTEB(tasks=["AmazonCounterfactualClassification",
                                 # "AmazonReviewsClassification",
                                 # "LivedoorNewsClustering.v2",
                                 # "MewsC16JaClustering",
                                 # "MIRACLReranking",
-                                # "NLPJournalAbsIntroRetrieval",
-                                # "NLPJournalTitleAbsRetrieval",
-                                # "NLPJournalTitleIntroRetrieval",
+                                "NLPJournalAbsIntroRetrieval",
+                                "NLPJournalTitleAbsRetrieval",
+                                "NLPJournalTitleIntroRetrieval",
                                 "JSICK",
                                 "JSTS"],
                                 task_langs=["jpn"],)
-    scores = evaluation.run(model, output_folder=output_folder,
-                            batch_size=args.batch_size, num_workers=args.num_workers,trust_remote_code=True)
-    import pdb; pdb.set_trace()
-    print(f"Evaluation results saved to {output_folder}")
-
+    import warnings
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        # MTEBの評価を実行
+        scores = evaluation.run(model, output_folder=output_folder,
+                                batch_size=args.batch_size, num_workers=args.num_workers,trust_remote_code=True,verbosity=0)
+    mteb_dict = {score.task_name: score.get_score() for score in scores}
+    print("Evaluation scores:")
+    for task_name, score in mteb_dict.items():
+        print(f"{task_name}: {score}")
+    with open(output_folder / "scores.txt", "w") as f:
+        f.write(f"|")
+        for task_name in mteb_dict.keys():
+            f.write(f"{task_name}|")
+        f.write(f"\n|")
+        for score in mteb_dict.values():
+            f.write(f"{score}|")
+    print(f"Scores saved to {output_folder / 'scores.txt'}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run MTEB evaluation on a model.")
