1ffaff12645b49ec3c912f746c7e91d12f099164
yano0 <yanonay3@gmail.com>
2025-08-08 00:52:08 +0900

    script変更

--- script/genkai_train.sh
+++ script/genkai_train.sh
@@ -8,14 +8,14 @@ module load cuda cudnn nccl gcc
 
 nvidia-smi
 
-for loss_type in "mse" "kld" "ckd" "taid-ckd" "taid-mse" "taid-kld"; do
+for loss_type in "kld" "ckd" "taid-ckd" "taid-kld"; do
     uv run python train.py \
         --student_model cl-nagoya/ruri-v3-pt-30m \
         --teacher_model Qwen/Qwen3-Embedding-4B \
         --data_dir data \
         --dataset_name sample_10BT_100000 \
         --output_dir output \
-        --batch_size 64 \
+        --batch_size 128 \
         --num_epochs 3 \
         --max_length 4096 \
         --val_check_interval 0.1 \
