2a881552a0948314bfbee1a46a4f2e32712fc396
yano0 <yanonay3@gmail.com>
2025-08-13 17:55:58 +0900

    chore

--- script/genkai_eval.sh
+++ script/genkai_eval.sh
@@ -9,7 +9,7 @@ module load cuda cudnn nccl gcc
 nvidia-smi
 uv sync
 for model_name in "answerdotai/ModernBERT-base" "answerdotai/ModernBERT-large"; do
-    uv run python eval.py \
+    uv run python eval_mteb.py \
         --model_name $model_name \
         --batch_size 64
 done
