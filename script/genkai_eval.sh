8c99563a9fa9b42a1b71512afd2bdc79b7a194d6
yano0 <yanonay3@gmail.com>
2025-08-14 15:35:02 +0900

    edit script

--- script/genkai_eval.sh
+++ script/genkai_eval.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 #PJM -L rscgrp=b-batch
 #PJM -L gpu=1
-#PJM -L elapse=01:00:00
+#PJM -L elapse=3:00:00
 #PJM -j
 
 module load cuda cudnn nccl gcc
 
 nvidia-smi
 uv sync
-for model_name in "answerdotai/ModernBERT-base" "answerdotai/ModernBERT-large"; do
+for model_name in "answerdotai/ModernBERT-large"; do
     uv run python eval_mteb.py \
         --model_name $model_name \
-        --batch_size 64
+        --batch_size 128
 done
