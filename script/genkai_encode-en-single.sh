00484f538bf55286d441991c8e35ff312e2a6cb1
yano0 <yanonay3@gmail.com>
2025-08-14 14:50:12 +0900

    add script

---
+++ script/genkai_encode-en-single.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#PJM -L rscgrp=b-batch
+#PJM -L gpu=1
+#PJM -L elapse=07:00:00
+#PJM -j
+
+module load cuda cudnn nccl gcc
+
+# GPU情報を表示
+nvidia-smi
+
+uv sync
+
+uv run python src/preprocess/triplet-en_encode.py \
+    --teacher_model "Qwen/Qwen3-Embedding-4B" \
+    --long_batch_size 4 \
+    --short_batch_size 128 \
+    --max_length 4096 \
+    --sample_size 1_000_000 \
+    --disable_multigpu
