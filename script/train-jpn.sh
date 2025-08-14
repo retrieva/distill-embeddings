070c1bdcbdf0d552f0d4dbdcd949d313beb4b08f
yano0 <yanonay3@gmail.com>
2025-08-14 15:20:12 +0900

    chore

--- script/train-jpn.sh
+++ script/train-jpn.sh
@@ -11,13 +11,14 @@ for loss_type in "ckd"; do
         --student_model cl-nagoya/ruri-v3-pt-30m \
         --teacher_model Qwen/Qwen3-Embedding-4B \
         --data_dir data \
-        --dataset_name sample_10BT_1000 \
+        --dataset_name 1000 \
         --output_dir output/result \
         --batch_size 16 \
         --num_epochs 1 \
         --max_length 4096 \
         --val_check_interval 1 \
         --log_every_n_steps 1 \
+        --language jpn \
         --mteb_eval \
         --loss_type "$loss_type"
 done
