5fe24775f7868c29e5ee83a5e0e9aef0e835dbc2
yano0 <yanonay3@gmail.com>
2025-08-18 14:43:53 +0900

    0.6Bモデルでのエンコードする

--- script/genkai_encode-en.sh
+++ script/genkai_encode-en.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PJM -L rscgrp=b-batch
 #PJM -L gpu=4
-#PJM -L elapse=01:00:00
+#PJM -L elapse=10:00:00
 #PJM -j
 
 module load cuda cudnn nccl gcc
@@ -15,8 +15,8 @@ uv sync
 
 # マルチGPUでエンコーディング実行
 uv run python src/preprocess/triplet-en_encode.py \
-    --teacher_model "Qwen/Qwen3-Embedding-4B" \
-    --long_batch_size 2 \
-    --short_batch_size 64 \
+    --teacher_model "Qwen/Qwen3-Embedding-0.6B" \
+    --long_batch_size 8 \
+    --short_batch_size 128 \
     --max_length 4096 \
     --sample_size 1_000_000
