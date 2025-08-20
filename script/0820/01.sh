8efbec06d7d5c3764fcec083a765be1b74ba8d3f
yano0 <yanonay3@gmail.com>
2025-08-20 11:44:09 +0900

    add script

--- script/0820/01.sh
+++ script/0820/01.sh
@@ -10,7 +10,7 @@ module load cuda cudnn nccl gcc
 nvidia-smi
 export SSL_CERT_FILE=$(uv run python -c "import certifi; print(certifi.where())")
 
-for loss_type in "kld"; do
+for loss_type in "kld" "ckd"; do
     uv run python train.py \
         --student_model answerdotai/ModernBERT-base \
         --teacher_model Qwen/Qwen3-Embedding-0.6B \
@@ -23,7 +23,26 @@ for loss_type in "kld"; do
         --val_check_interval 0.1 \
         --log_every_n_steps 1 \
         --mteb_eval \
+        --use_pos \
         --language "eng" \
         --taid_t_start 0.6 \
         --loss_type "$loss_type"
 done
+
+# for loss_type in "kld" "ckd"; do
+#     uv run python train.py \
+#         --student_model answerdotai/ModernBERT-base \
+#         --teacher_model Qwen/Qwen3-Embedding-0.6B \
+#         --data_dir data \
+#         --dataset_name 1000000 \
+#         --output_dir output/result \
+#         --batch_size 256 \
+#         --num_epochs 2 \
+#         --max_length 512 \
+#         --val_check_interval 0.1 \
+#         --log_every_n_steps 1 \
+#         --mteb_eval \
+#         --language "eng" \
+#         --taid_t_start 0.6 \
+#         --loss_type "$loss_type"
+# done
