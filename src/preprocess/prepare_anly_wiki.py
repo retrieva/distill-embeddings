eb416c2d353c8508604c63af1d3b522ed3bf4958
yano0 <yanonay3@gmail.com>
2025-08-25 16:59:32 +0900

    wiki datdasetの準備

---
+++ src/preprocess/prepare_anly_wiki.py
@@ -0,0 +1,9 @@
+from datasets import load_dataset
+from pathlib import Path
+data = load_dataset("graelo/wikipedia","20230601.en",split="train",trust_remote_code=True)
+data = data.shuffle(seed=42).select(range(10000))
+data = data.map(lambda x: {"text": x["title"].strip() + " " + x["text"].strip()})
+data = data.remove_columns([c for c in data.column_names if c != "text"])
+
+Path("data/anly-wiki/en").mkdir(parents=True, exist_ok=True)
+data.save_to_disk("data/anly-wiki/en")
