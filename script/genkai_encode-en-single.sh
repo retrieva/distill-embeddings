42641263e4ab3d51eac4f94001b68dabd53bea3c
yano0 <yanonay3@gmail.com>
2025-08-26 09:17:10 +0900

    間違えた

--- script/genkai_encode-en-single.sh
+++ script/genkai_encode-en-single.sh
@@ -12,7 +12,7 @@ nvidia-smi
 uv sync
 
 uv run python src/preprocess/triplet-en_encode.py \
-    --teacher_model "Qwen/Qwen3-Embedding-0.6B" \
+    --teacher_model "Qwen/Qwen3-Embedding-4B" \
     --long_batch_size 2 \
     --short_batch_size 64 \
     --max_length 4096 \
