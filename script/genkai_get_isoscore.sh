76b77c66acf5e93e478a738433501f1864498e2f
yano0 <yanonay3@gmail.com>
2025-08-26 17:50:41 +0900

    chore

--- script/genkai_get_isoscore.sh
+++ script/genkai_get_isoscore.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#PJM --interact
-#PJM -L rscgrp=b-inter
+#PJM --batch
+#PJM -L rscgrp=b-batch
 #PJM -L gpu=1
-#PJM -L elapse=1:00:00
+#PJM -L elapse=2:00:00
 module load cuda cudnn nccl gcc
 
 nvidia-smi
@@ -10,14 +10,14 @@ export SSL_CERT_FILE=$(uv run python -c "import certifi; print(certifi.where())"
 
 uv sync
 
-uv run python get_isoscore.py \
+uv run python get_id_iso.py \
     --model_name "Qwen/Qwen3-Embedding-4B"
 
-uv run python get_isoscore.py \
+uv run python get_id_iso.py \
     --model_name "output/result/answerdotai_ModernBERT-base/Qwen_Qwen3-Embedding-4B/1000000/e10_bs128_wsd5e-05_kld_w-pos/checkpoints/epoch=09.ckpt/consolidated_model.pt/pytorch_model.bin"
 
-uv run python get_isoscore.py \
+uv run python get_id_iso.py \
     --model_name "output/result/answerdotai_ModernBERT-base/Qwen_Qwen3-Embedding-4B/1000000/e4_bs128_wsd5e-05_kld_w-pos/checkpoints/epoch=03.ckpt/consolidated_model.pt/pytorch_model.bin"
 
-uv run python get_isoscore.py \
+uv run python get_id_iso.py \
     --model_name "Qwen/Qwen3-Embedding-0.6B"
