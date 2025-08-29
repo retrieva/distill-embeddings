ddfa169479afe5351ff8bcfb07e5549712e70028
yano0 <yanonay3@gmail.com>
2025-08-29 15:03:44 +0900

    eval!

--- script/genkai_eval.sh
+++ script/genkai_eval.sh
@@ -8,7 +8,7 @@ module load cuda cudnn nccl gcc
 
 nvidia-smi
 uv sync
-for model_name in "answerdotai/ModernBERT-large"; do
+for model_name in "distill-embeddings/output/result/nomic-ai_modernbert-embed-base-unsupervised/Qwen_Qwen3-Embedding-4B/794554/gte_e3_bs128_wsd5e-05_taid-kld_w-pos/checkpoints/epoch=02.ckpt"; do
     uv run python eval_mteb.py \
         --model_name $model_name \
         --batch_size 128
