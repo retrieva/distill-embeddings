109084534bf0d6430842f6f4f543d162236cf2ae
yano0 <yanonay3@gmail.com>
2025-09-02 12:12:53 +0900

    fix error

--- script/0902/02.sh
+++ script/0902/02.sh
@@ -10,7 +10,7 @@ module load cuda cudnn nccl gcc
 nvidia-smi
 uv sync
 for model_name in "nomic-ai/modernbert-embed-base-unsupervised"; do
-    uv run python -m src.evaluation.run_mteb.py \
+    uv run python -m src.evaluation.run_mteb \
         --model_name 'output/result/nomic-ai_modernbert-embed-base-unsupervised/Qwen_Qwen3-Embedding-4B/794554/gte_e3_bs128_cosine5e-05_kld_w-pos/checkpoints/epoch=02.ckpt' \
         --batch_size 128 \
         --benchmark_name "MTEB(eng, v2)"
