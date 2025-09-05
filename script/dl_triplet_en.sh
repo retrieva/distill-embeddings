95a9f3ec8fcdc8db606eeb274c0de4192d7bb0a0
yano0 <yanonay3@gmail.com>
2025-09-05 13:14:18 +0900

    fix path

--- script/dl_triplet_en.sh
+++ script/dl_triplet_en.sh
@@ -1,6 +1,4 @@
-# mkdir data/triplet-en
-# cd data/triplet-en
-# hf download sentence-transformers/embedding-training-data \
-#   --repo-type dataset \
-#   --local-dir ./data/triplet-en
-gzip -dvf ./data/triplet-en/*.gz
+uv run hf download sentence-transformers/embedding-training-data \
+  --repo-type dataset \
+  --local-dir ./data/gte_plus-eng/triplet/raw
+gzip -dvf ./data/gte_plus-eng/triplet/raw/*.gz
